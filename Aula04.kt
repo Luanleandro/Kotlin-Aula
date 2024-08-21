df015e39ae6e44753fb02acaa302a6785d805ae3
Gabriel de Oliveira Pereira <2302899@SALAS.AULAS>
2024-08-21 19:36:05 -0300

    teste

---
+++ Aula04.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val a = 2
+    val b = 2
+
+    val resultado = a + b
+    println(resultado)
+
+    var usuario = "Gabriel Oliveira"
+    var texto = "Boa tarde, seja bem-vindo, $usuario ao sistema "
+    println(texto)
+
+    val text2 = """ Exemplo de texto 
+                    com mais de uma 
+                    linha 
+                """
+    println(text2)
+}
